f516ec3ed1b631f7c9608ffe700e950ab18ac41a
ERROR <72090728+git404x@users.noreply.github.com>
2026-08-15 14:46:14 +0530

    fix: use CSR based project

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // explicitly allow lan connections so react hydrated correctly
   allowedDevOrigins: ["*", "192.168.29.236"],
+  // static out
+  output: "export",
 };
 
 export default nextConfig;
